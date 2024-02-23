e41a0ad8d57cfecf981d45771962f2125ad187a2
Jeimy <jeimymorales234@gmail.com>
2024-02-23 08:31:02 -0500

    Funcional el post con la base de datos

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { RequestLogDocument } from '../modules/request-log/request-log.shema'; // Importa el tipo de documento del esquema de registro de solicitudes
-
+import { RequestLogDocument } from '../modules/request-log/request-log.shema'; // Importa el 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
@@ -10,5 +9,12 @@ export class PostsController {
   async getAllPosts(): Promise<RequestLogDocument[]> {
     // Usa el tipo de documento del esquema de registro de solicitudes
     return this.postsService.getAllPosts();
+  }
+
+  @Post()
+  async createPost(@Body() postData: any): Promise<any> {
+    // Aquí puedes manejar la lógica para crear un nuevo post utilizando el servicio
+    // Por ejemplo:
+    return this.postsService.createPost(postData);
   }
 }
